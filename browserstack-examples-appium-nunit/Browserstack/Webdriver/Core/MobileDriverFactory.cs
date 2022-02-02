688664ac5440208a38a9ff8d1ae3af614ed24171
Bhagyesh Nanwani <75517861+Bhagyesh22@users.noreply.github.com>
2022-02-02 21:49:53 +0530

    Update MobileDriverFactory.cs

--- browserstack-examples-appium-nunit/Browserstack/Webdriver/Core/MobileDriverFactory.cs
+++ browserstack-examples-appium-nunit/Browserstack/Webdriver/Core/MobileDriverFactory.cs
@@ -20,6 +20,7 @@ namespace BrowserStack.WebDriver.Core
         private readonly string BROWSERSTACK_USERNAME = "BROWSERSTACK_USERNAME";
         private readonly string BROWSERSTACK_ACCESS_KEY = "BROWSERSTACK_ACCESS_KEY";
         private readonly string BROWSERSTACK_ANDROID_APP_ID = "BROWSERSTACK_ANDROID_APP_ID";
+        private readonly string BROWSERSTACK_APP_ID = "abcd";
         private readonly string BROWSERSTACK_IOS_APP_ID = "BROWSERSTACK_IOS_APP_ID";
         private readonly string BUILD_ID = "BROWSERSTACK_BUILD_NAME";
         private readonly string DEFAULT_BUILD_NAME = "browserstack-examples-appium_nunit";
